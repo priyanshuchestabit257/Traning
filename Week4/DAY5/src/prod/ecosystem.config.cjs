8bf27d5457c2db0f1e6dec6ed4f3365aa0d86483
Priyanshu <priyanshuc.hestabit@gmail.com>
2026-01-06 19:40:36 +0530

    afte- verification

---
+++ Week4/DAY5/src/prod/ecosystem.config.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: "backend-api",
+      script: "index.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000
+      }
+    },
+    {
+      name: "email-worker",
+      script: "jobs/email.job.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
